38de629b03e2d438483b056f71f663ded176b28e
Mauve-negi <116134417+Mauve-negi@users.noreply.github.com>
2023-03-09 03:40:49 +0000

    [Finish]3-9

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,29 +4,23 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
-  # def create
-  #   # # １.&2. データを受け取り新規登録するためのインスタンス作成
-  #   # list = List.new(list_params)
-  #   # # 3. データをデータベースに保存するためのsaveメソッド実行
-  #   # list.save
-  #   # # 4. トップ画面へリダイレクト
-  #   # redirect_to list_path(list.id)
-    
-  #   @list = List.new(list_params)
-  #   if @list.save
-  #     redirect_to list_path(@list.id)
-  #   else
-  #     render :new
-  #   end
-    
-  # end
-  
   def create
+    # # １.&2. データを受け取り新規登録するためのインスタンス作成
+    # list = List.new(list_params)
+    # # 3. データをデータベースに保存するためのsaveメソッド実行
+    # list.save
+    # # 4. トップ画面へリダイレクト
+    # redirect_to list_path(list.id)
+
     @list = List.new(list_params)
     if @list.save
       redirect_to list_path(@list.id)
     else
-      render :index #<= new から indexに変更    end  
+      render :new
+      # @lists = List.all
+      # render :index #<= new から indexに変更
+    end
+
   end
 
   def index
@@ -41,17 +35,17 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
-    redirect_to list_path(list.id)  
+    redirect_to list_path(list.id)
   end
-  
+
   def destroy
     list = List.find(params[:id])  # データ（レコード）を1件取得
     list.destroy  # データ（レコード）を削除
-    redirect_to '/lists'  # 投稿一覧画面へリダイレクト  
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト
   end
 
   private
